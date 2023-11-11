278ea4da65c945a22e382220714372c1c22939d4
Mohtisham-cs01 <117306878+Mohtisham-cs01@users.noreply.github.com>
2023-11-11 15:53:08 +0500

    Initial commit

---
+++ jwtAuthenticationApi/Auth/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿
+namespace jwtAuthenticationApi.Auth
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
